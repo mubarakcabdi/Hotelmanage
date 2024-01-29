2d81e46403e8c3c92c80c5ab22e6d51500f69855
mubarak <hp@DESKTOP-9GQQRHD>
2024-01-29 19:02:10 +0300

    change color button save

--- Hotelmanage/Pages/employee/Edit_employee.cshtml.cs
+++ Hotelmanage/Pages/employee/Edit_employee.cshtml.cs
@@ -38,7 +38,7 @@ namespace Hotelmanage.Pages.employee
             {
                 SqlConnection connection = new SqlConnection(connectionstring);
                 connection.Open();
-                string sql = "update emplooyee set"+
+                string sql = "update employee set " +
                      "name = '" + name + "'," +
                     "gender = '" + gender + "'," +
                     "address = '" + address + "'," +
